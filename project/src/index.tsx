f5dbc9984e8c520bc4119c229b9d1cd788a16b4a
Irina Viktorova <irinasafe@icloud.com>
2022-02-12 23:56:47 +0300

    Добавила компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const FilmsData = {
+  filmName: 'The Grand Budapest Hotel',
+  filmGenre: 'Drama',
+  filmYear: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      filmName = {FilmsData.filmName}
+      filmGenre = {FilmsData.filmGenre}
+      filmYear = {FilmsData.filmYear}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
